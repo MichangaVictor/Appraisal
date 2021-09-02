6cbf22cd86faa47ff26e59e357478ba8e8846734
MichangaVictor <onyango.vic@yandex.com>
2021-09-02 18:55:13 +0300

    updates....create forms

--- appraisal/hr/forms.py
+++ appraisal/hr/forms.py
@@ -31,12 +31,12 @@ class EmployeeForm(forms.ModelForm):
         label='Username',
         required=True
     )
-    employee_email=forms.EmailField(
+    employee_email = forms.EmailField(
         required=True,
         label='Employee Email'
     )
 
-    employee_contact_no=forms.CharField(
+    employee_contact_no = forms.CharField(
         required=True,
         label='Contact Number'
     )
@@ -63,7 +63,7 @@ class EmployeeForm(forms.ModelForm):
                     bootstrap.FormActions(
                         layout.Submit('submit', 'Submit', css_class='btn btn-primary'),
                         layout.HTML(
-                            f'<a href="{reverse("employees")}" class="btn btn-outline-secondary">{("Cancel")}</a>')
+                            f'<a href="{reverse("employee")}" class="btn btn-outline-secondary">{("Cancel")}</a>')
                     ),
                     css_class='row'
                 ),
@@ -74,5 +74,249 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = ['employee_first_name', 'employee_middle_name', 'employee_last_name', 'employee_username',
-                  'employee_gender','employee_role', 'employee_email', 'employee_contact_no']
+                  'employee_gender', 'employee_role', 'employee_email', 'employee_contact_no']
 
+
+class DesignationForm(forms.ModelForm):
+    designation_code = forms.CharField(
+        label='Designation Code',
+        required=True
+    )
+    designation_name = forms.CharField(
+        label='Designation Name',
+        required=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(DesignationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Div('designation_code', css_class="col-sm-3"),
+                    Div('designation_name', css_class='col-sm-9'),
+                    bootstrap.FormActions(
+                        layout.Submit('submit', 'Submit', css_class='btn btn-primary'),
+                        layout.HTML(
+                            f'<a href="{reverse("designation")}" class="btn btn-outline-secondary">{("Cancel")}</a>')
+                    ),
+                    css_class='row'
+                ),
+                css_class="container"
+            )
+        )
+
+    class Meta:
+        model = Designation
+        fields = ['designation_code', 'designation_name']
+
+
+class MinistryForm(forms.ModelForm):
+    ministry_code = forms.CharField(
+        label='Ministry Code',
+        required=True
+    )
+    ministry_name = forms.CharField(
+        label='Ministry Name',
+        required=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(MinistryForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Div('ministry_code', css_class="col-sm-3"),
+                    Div('ministry_name', css_class='col-sm-9'),
+                    bootstrap.FormActions(
+                        layout.Submit('submit', 'Submit', css_class='btn btn-primary'),
+                        layout.HTML(
+                            f'<a href="{reverse("ministry")}" class="btn btn-outline-secondary">{("Cancel")}</a>')
+                    ),
+                    css_class='row'
+                ),
+                css_class="container"
+            )
+        )
+
+    class Meta:
+        model = Ministry
+        fields = ['ministry_code', 'ministry_name']
+
+
+class DutyStationForm(forms.ModelForm):
+    duty_station_code = forms.CharField(
+        label='Duty Station Code',
+        required=True
+    )
+    duty_station_name = forms.CharField(
+        label='Duty Station Name',
+        required=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(DutyStationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Div('duty_station_code', css_class="col-sm-3"),
+                    Div('duty_station_name', css_class='col-sm-9'),
+                    bootstrap.FormActions(
+                        layout.Submit('submit', 'Submit', css_class='btn btn-primary'),
+                        layout.HTML(
+                            f'<a href="{reverse("duty-station")}" class="btn btn-outline-secondary">{("Cancel")}</a>')
+                    ),
+                    css_class='row'
+                ),
+                css_class="container"
+            )
+        )
+
+    class Meta:
+        model = DutyStation
+        fields = ['duty_station_code', 'duty_station_name']
+
+
+class SectionForm(forms.ModelForm):
+    section_code = forms.CharField(
+        label='Section Code',
+        required=True
+    )
+    section_name = forms.CharField(
+        label='Section Name',
+        required=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(SectionForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Div('section_code', css_class="col-sm-3"),
+                    Div('section_name', css_class='col-sm-9'),
+                    bootstrap.FormActions(
+                        layout.Submit('submit', 'Submit', css_class='btn btn-primary'),
+                        layout.HTML(
+                            f'<a href="{reverse("section")}" class="btn btn-outline-secondary">{("Cancel")}</a>')
+                    ),
+                    css_class='row'
+                ),
+                css_class="container"
+            )
+        )
+
+    class Meta:
+        model = Section
+        fields = ['section_code', 'section_name']
+
+
+class EmploymentTypeForm(forms.ModelForm):
+    employment_type_code = forms.CharField(
+        label='Employment Type Code',
+        required=True
+    )
+    employment_type = forms.CharField(
+        label='Employment Type',
+        required=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(EmploymentTypeForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Div('employment_type_code', css_class="col-sm-3"),
+                    Div('employment_type', css_class='col-sm-9'),
+                    bootstrap.FormActions(
+                        layout.Submit('submit', 'Submit', css_class='btn btn-primary'),
+                        layout.HTML(
+                            f'<a href="{reverse("employment_type")}" class="btn btn-outline-secondary">{("Cancel")}</a>')
+                    ),
+                    css_class='row'
+                ),
+                css_class="container"
+            )
+        )
+
+    class Meta:
+        model = EmploymentType
+        fields = ['employment_type_code', 'employment_type']
+
+
+class JobGroupForm(forms.ModelForm):
+    job_group_code = forms.CharField(
+        label='Job Group Code',
+        required=True
+    )
+    job_group = forms.CharField(
+        label='Job Group',
+        required=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(JobGroupForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Div('job_group_code', css_class="col-sm-3"),
+                    Div('job_group', css_class='col-sm-9'),
+                    bootstrap.FormActions(
+                        layout.Submit('submit', 'Submit', css_class='btn btn-primary'),
+                        layout.HTML(
+                            f'<a href="{reverse("job_group")}" class="btn btn-outline-secondary">{("Cancel")}</a>')
+                    ),
+                    css_class='row'
+                ),
+                css_class="container"
+            )
+        )
+
+    class Meta:
+        model = JobGroup
+        fields = ['job_group_code', 'job_group']
+
+
+class DirectorateForm(forms.ModelForm):
+    directorate_code = forms.CharField(
+        label='Directorate Code',
+        required=True
+    )
+    directorate_name = forms.CharField(
+        label='Directorate Name',
+        required=True
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(DirectorateForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Div('directorate_code', css_class="col-sm-3"),
+                    Div('directorate_name', css_class='col-sm-9'),
+                    bootstrap.FormActions(
+                        layout.Submit('submit', 'Submit', css_class='btn btn-primary'),
+                        layout.HTML(
+                            f'<a href="{reverse("directorate")}" class="btn btn-outline-secondary">{("Cancel")}</a>')
+                    ),
+                    css_class='row'
+                ),
+                css_class="container"
+            )
+        )
+
+    class Meta:
+        model = Directorate
+        fields = ['directorate_code', 'directorate_name']
